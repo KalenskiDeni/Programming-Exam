d58d19b72a2a1cdf25c1c2a76295f59a11ed10e1
KalenskiDeni <kallenski.deni@gmail.com>
2024-05-27 11:22:04 +0200

    fetch teachers functionality

--- app.js
+++ app.js
@@ -3,6 +3,34 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getPosts();
+  console.log(posts);
+}
+
+async function getPosts() {
+  const response = await fetch (
+    "https://headless.cederdorff.dk/wp-json/wp/v2/posts"
+  );
+  const data = await response.json();
+  return data;
+}
+
+function displayPostsGrid (posts) {
+  const postsGrid = document.querySelector("#posts-grid");
+  
+  for (const post of posts) {
+    postsGrid.insertAdjacentHTML(
+      "beforeend",
+      /*html*/ `
+      <article class="grid-item">
+        <img src="${post.acf.image}" alt="${post.title.rendered}" />
+        <h2>${post.title.rendered}</h2>
+      </article>
+    `
+    );
+  
+  }
+  
 }
