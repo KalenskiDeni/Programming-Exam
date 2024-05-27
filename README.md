6a3ff263cd7b5803b42f9d146b778308e60511f2
Deni Kalenski <144707734+KalenskiDeni@users.noreply.github.com>
2024-05-27 10:40:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Template
+
+Simple web frontend project template with `index.html`, `app.css` and `app.js`.
---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+window.addEventListener("load", initApp); // When the page is loaded, run initApp function
+
+// Function to initialize the Web App
+function initApp() {
+  console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+}
